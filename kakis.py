217c674d72ec0f377ada973dd3d0c9024d365bb0
henrietei <113431841+henrietei@users.noreply.github.com>
2022-09-20 13:53:59 +0300

    labojums

---
+++ kakis.py
@@ -0,0 +1,17 @@
+class Cat:
+    name=None
+    age=None
+    isHappy=None
+
+cat1=Cat()
+cat1.name="Milka"
+cat1.age="11"
+cat1.isHappy=True
+print(cat1.name, cat1.age, cat1.isHappy)
+
+
+cat2=Cat()
+cat2.name="pucins"
+cat2.age=3
+cat2.isHappy=False
+print(cat2.name, cat2.age, cat2.isHappy)
